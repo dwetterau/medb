dc11a0d6a5f5732129f296c9acb37943773df273
David Wetterau <david.wetterau@gmail.com>
2017-12-17 01:08:21 -0800

    initial ui framework added

---
+++ ui/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PagedrawGeneratedPage from './pagedraw/app'
+
+class App extends Component {
+  render() {
+    return (
+        <PagedrawGeneratedPage />
+    );
+  }
+}
+
+export default App;
